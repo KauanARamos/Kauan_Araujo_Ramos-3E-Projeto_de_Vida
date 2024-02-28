833ef03dd486b732eabf20eddc37afd63e012af9
Kauan Ramos <101677584+KauanARamos@users.noreply.github.com>
2024-02-28 17:04:57 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const botoes = document.querySelectorAll(".botao");
+
+/*remove e adiciona a classe "ativo" aos botoes*/
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+    }
+}
